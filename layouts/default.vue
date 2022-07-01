84d506b2358caa75e08be34a06728e8287dae617
samoorai <hirotakatech@gmail.com>
2022-07-01 16:07:37 +0200

    Added font awsesome

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="container mt-3">
+      <nav class="navbar navbar-expand-md navbar-light bg-main">
+        <nuxt-link class="navbar-brand" to="/">Home</nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="true"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/work">Work</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/music">Music</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+.bg-main{
+  background-color: #06D6A0;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="assets/img/avatar-without-bg.png" class="rounded-circle avatar-image">
+      </div>
+      <div class="col-md-6">
+        <h1 class="display-4">I'm Guglielmo Bartelloni</h1>
+        <p class="paragrafo">Software Developer and Music lover based in Italy.</p>
+
+        <font-awesome-icon :icon="['fas', 'home']"/>
+          </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style scoped>
+.avatar-image{
+  border: 8px dotted white;
+  border-radius: 8000px;
+  width: 80%;
+}
+.paragrafo{
+  font-size: 18pt;
+  font-weight: lighter;
+  color: black;
+}
+
+</style>

31f1092dc86a911063a035054d0dd23c6bb52c36
samoorai <hirotakatech@gmail.com>
2022-07-03 19:06:53 +0200

    Rimosso audio player

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [
-        { src: '~plugins/vue-audio-player', ssr: false }
+        // { src: '~plugins/vue-audio-player', ssr: false }
     ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
